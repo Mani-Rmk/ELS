f36383015fcf023818ff81bde87f5a88bc549ee2
Manikandan <r.manikandan3112@gmail.com>
2025-09-22 12:20:52 +0530

    Update config.py
    email update

--- employee_leave_system/core/config.py
+++ employee_leave_system/core/config.py
@@ -2,6 +2,6 @@
 # config.py
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-EMAIL_ADDRESS = "mailcheckvt@gmail.com"
+EMAIL_ADDRESS = "paste your smtp email"
 EMAIL_PASSWORD = "paste your app password here"
 
